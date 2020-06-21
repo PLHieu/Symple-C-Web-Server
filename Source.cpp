825debbb05b696a851f964c4d1d576b0fb0be11e
PLHieu <hieuflong@gmail.com>
2020-06-21 13:13:10 +0700

    load image

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <time.h>
 #include <string>
+#include <errno.h>
 
 #pragma comment (lib, "ws2_32.lib")
 
@@ -21,7 +22,107 @@ using namespace std;
 //void handleMessage(char* buff);
 
 
-void handleMessage(char *&buff)
+void handleMessage(char*& buff)
+{
+
+	istringstream data(buff);
+	vector<string> words((istream_iterator<string>(data)), istream_iterator<string>());
+
+	// chua response
+	ostringstream resp;
+	// file_name chua ten cua file tra ve , dung de luu trong response 
+	string file_name = "";
+	int ReturnCode;
+	FILE *f = NULL ;
+
+	if (words[0] == "GET") {
+		// lay ten file
+		file_name = words[1];
+		if (file_name == "/")
+		{	
+			file_name = "/index.html";
+		}
+
+		file_name.insert(0,"..");
+		 //const char* a = file_name.c_str() +1;
+		// mo file duo dang nhi phan
+		f = fopen(file_name.c_str(), "rb");
+
+		// neu nhu mo file khong duoc thi  -> 404.html, mo lai file do
+		if (f == NULL) {
+			cout << strerror(errno); 
+			file_name = "../404.html";
+			f = fopen(file_name.c_str(), "wb");
+		}
+	}
+	else if (words[0] == "POST") {
+
+		string request_str = data.str();
+
+		// xac thuc username va password
+		int pos_username = request_str.find("username");
+		int pos_end = request_str.find("Í");
+		int pos_password = request_str.find("password");
+
+		string username = request_str.substr(pos_username + 9, pos_password - pos_username - 10);
+		string pass = request_str.substr(pos_password + 9, pos_end - pos_password - 9);
+
+		if (username == "admin" && pass == "admin") {
+			file_name = "../info.html";
+		}
+		else {
+			file_name = "../404.html";
+		}
+
+		f = fopen(file_name.c_str(), "rb");
+
+	}
+
+
+	// tien hanh ghi du lieu vao response	
+	// nap du lieu tu trong file vao trong htmlfile_content
+	//string str((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
+	//file_name = str;
+	//int result_length =
+	ReturnCode = 200;
+	//f.close();
+
+
+	// tinh toan length cua file 
+	fseek(f, 0, SEEK_END);
+	int fileLength = ftell(f);
+	rewind(f);
+
+
+	// nap header truoc
+	// response cho phuong thuc GET
+	resp << "HTTP/1.1 " << ReturnCode << " OK\r\n";
+	resp << "Cache-Control: no-cache, private\r\n";
+	////resp << "Last-Modified: " << ;
+	//	// check xem co phai la anh hay khong 
+	if (file_name == "../hvh.jpg" or file_name == "../plh.jpg") {
+		resp << "Content-Type: image/jpg\r\n";
+	}
+	resp << "Content-Transfer-Encoding: binary\r\n";
+	//resp << "Connection: close\r\n";
+	resp << "Content-Length: " << fileLength << "\r\n";
+	//resp << "Content-Type: text/html\r\n";
+	resp << "\r\n";
+
+	// nap du lieu vao trong buff
+
+
+	//cout << resp.str();
+	// dua header response vao trong buff
+	strcpy(buff, resp.str().c_str());
+	char* bufftemp = new char[10000];
+	fread(bufftemp, 1, fileLength, f);
+	strcpy(buff, strcat(buff, bufftemp)) ;
+	cout << buff;
+	fclose(f);
+}
+
+void handleMessage_old(char *&buff)
 {
 
 	istringstream data(buff);
@@ -119,8 +220,8 @@ void handleMessage(char *&buff)
 void handleClient(SOCKET clientSocket, char* stillwork_flag)
 {
 	//transmit file
-	int buffsize = 10000;
-	char* buff = new char[10000];
+	int buffsize = 20000;
+	char* buff = new char[20000];
 	int r;
 	do {
 		r = recv(clientSocket, buff, buffsize, 0);
